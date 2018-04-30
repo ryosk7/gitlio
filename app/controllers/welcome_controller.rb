351278ef37f9599e93ea9497efbc50c4e15c86f5
Ryosuke Uchida <uchiryo7@gmail.com>
2018-04-30 14:01:00 +0900

    displayed user repos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,10 @@ class WelcomeController < ApplicationController
     # @users.each do |info|
     #   @client = Octokit::Client.new(:oauth_token => info.oauth_token)
     # end
-    client = Octokit::Client.new(:oauth_token => current_user.oauth_token)
-    @repos = client.refs[:repos].get.data
+    # client = Octokit::Client.new(:oauth_token => current_user.oauth_token)
+    # @repos = client.refs[:repos].get.data
+    @users.each do |user_info|
+      @client = Octokit::Client.new(:access_token => user_info.oauth_token)
+    end
   end
 end

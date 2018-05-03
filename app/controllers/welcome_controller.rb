99d8f583168a69113441a63598ca02324b1dc9f4
Ryosuke Uchida <uchiryo7@gmail.com>
2018-05-03 11:41:16 +0900

    create style for mobile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,16 +3,21 @@ class WelcomeController < ApplicationController
   end
   def index
     @users = User.all
-    # @users.each do |info|
-    #   @client = Octokit::Client.new(:oauth_token => info.oauth_token)
-    # end
-    # client = Octokit::Client.new(:oauth_token => current_user.oauth_token)
-    # @repos = client.refs[:repos].get.data
-
-    # @client = Octokit::Client.new(:access_token => current_user.oauth_token)
-
     @users.each do |user_info|
       @client = Octokit::Client.new(:access_token => user_info.oauth_token)
     end
+  end
+  def show
+    @user = User.find(params[:id])
+    @client = Octokit::Client.new(:access_token => @user.oauth_token)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :uid, :provider, :oauth_token,
+      :avatar_image, :repos_info
+    )
   end
 end

5ee00dbe5541001c9a5e19fc88afccf43d1f70c2
Ryosuke Uchida <uchiryo7@gmail.com>
2018-05-21 22:03:20 +0900

    success update user account(repos)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
   def callback
     auth = request.env['omniauth.auth']
-    user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth).save_repositories!
+    user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
+    user.repositories.destroy_all
+    user.save_repositories!
     session[:user_id] = user.id
     redirect_to welcome_index_path
   end

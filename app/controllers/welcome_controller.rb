b2c709de55874d4a63d817827ce569ff0ea5919b
Ryosuke Uchida <uchiryo7@gmail.com>
2018-06-25 01:22:12 +0900

    add nabbar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,16 +6,6 @@ class WelcomeController < ApplicationController
     # @users.each do |user_info|
     #   @client = Octokit::Client.new(:access_token => user_info.oauth_token)
     # end
-    @users.each do |user|
-      langs = user.repositories.map { |repo| repo.language.name }.compact
-      lang_hash = {}
-      langs.each do |lang|
-        count = langs.count(lang)
-        lang_hash[lang] = count
-      end
-      @chart_data = lang_hash
-    end
-
     # @chart_data = [['2014-04-01', 60], ['2014-04-02', 65], ['2014-04-03', 64]]
   end
   def show

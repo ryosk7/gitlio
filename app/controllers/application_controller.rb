83da1cc902aaa2e208b610dde0574bce288d4730
Ryosuke Uchida <uchiryo7@gmail.com>
2018-07-05 01:08:08 +0900

    [WIP] follow function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_user?
 
   private
 
   def current_user
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def current_user?(user)
+    user == @current_user
   end
 
   def logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#callback'
   get '/logout', to: 'sessions#destroy'
   resources :welcome, only: [:index, :show]
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   root 'welcome#top'
 end
